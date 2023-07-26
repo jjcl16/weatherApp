570d055936eb4c4111d2e6abf6e3556687f163ef
jjcl16 <caraballo2j@gmail.com>
2023-07-25 23:58:28 -0300

    add recommendations

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import createDom from "./createDom";
 import style from "./style.css";
 
 createDom();
+
+/***
+ * Recommendations:
+ *  change font 
+ *  background image depending on weather
+ *  prettier icons
+ */
